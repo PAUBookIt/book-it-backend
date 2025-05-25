3e8dfc439404e95c1826f2ceab8eaa85ed81b4b0
milobav1 <michael.iloba@pau.edu.ng>
2025-05-25 15:26:14 +0100

    feat: login

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,17 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfigAsync } from './infrastructure/database.config';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { ReservationsModule } from './modules/reservations/reservations.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, cache: true }),
     TypeOrmModule.forRootAsync(typeOrmConfigAsync),
+    AuthModule,
+    UsersModule,
+    ReservationsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
